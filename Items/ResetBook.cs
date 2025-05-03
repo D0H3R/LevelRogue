6ea62fcf2ad628a0572da9d1acc872115d697947
D0H3R <ldbdldbd3@gmail.com>
2025-05-03 21:05:31 +0500

    Add files via upload

---
+++ Items/ResetBook.cs
@@ -0,0 +1,83 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using Terraria.GameContent.Creative;
+using LevelRogue.UI;
+
+namespace LevelRogue.Items
+{
+    public class ResetBook : ModItem
+    {
+        public override LocalizedText DisplayName => Language.GetOrRegister("Книга Забвения");
+        public override LocalizedText Tooltip => Language.GetOrRegister("Возвращает уровень и умения вашего персонажа в изначальное состояние");
+
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 34;
+            Item.height = 38;
+            Item.maxStack = 9999;
+            Item.value = Item.sellPrice(gold: 1);
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.useTime = 30;
+            Item.useAnimation = 30;
+            Item.consumable = true;
+            Item.UseSound = SoundID.Item4;
+            Item.rare = ItemRarityID.Red;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            LevelPlayer modPlayer = player.GetModPlayer<LevelPlayer>();
+            return modPlayer.level > 1 || modPlayer.experience > 0 || modPlayer.statPoints > 0 ||
+                   modPlayer.spentMelee + modPlayer.spentRanged + modPlayer.spentMagic +
+                   modPlayer.spentSummon + modPlayer.spentRogue + modPlayer.spentPlayer > 0;
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            LevelPlayer modPlayer = player.GetModPlayer<LevelPlayer>();
+
+            modPlayer.level = 1;
+            modPlayer.experience = 0;
+            modPlayer.statPoints = 0;
+            modPlayer.bonusStatPoints = 0;
+
+            // Сбрасываем все бонусы характеристик
+            modPlayer.MeleeDamageBonus = 0;
+            modPlayer.bonusMeleeCrit = 0;
+            modPlayer.bonusMeleeSpeed = 0f;
+            modPlayer.bonusEndurance = 0f;
+
+            modPlayer.rangedDamageBonus = 0;
+            modPlayer.bonusRangedCrit = 0;
+
+            modPlayer.magicDamageBonus = 0;
+            modPlayer.bonusMagicCrit = 0;
+
+            modPlayer.summonDamageBonus = 0;
+            modPlayer.bonusSummonKnockback = 0f;
+
+            modPlayer.bonusHP = 0;
+            modPlayer.regenBonus = 0;
+            modPlayer.bonusLuck = 0f;
+            modPlayer.bonusAggro = 0;
+
+            // Сбрасываем затраченные очки
+			
+			modPlayer.UpdateWarriorRank();
+
+
+            Main.NewText("Ваш уровень и характеристики были полностью обнулены!", 255, 60, 60);
+
+            // Обновляем интерфейс
+
+            return true;
+        }
+    }
+}
---
+++ Items/StatResetItem.cs
@@ -0,0 +1,52 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using Terraria.GameContent.Creative;
+using LevelRogue.UI;
+
+namespace LevelRogue.Items
+{
+    public class ResetSkillBook : ModItem
+    {
+        public override LocalizedText DisplayName => Language.GetOrRegister("Том Перерождения");
+        public override LocalizedText Tooltip => Language.GetOrRegister("Позволяет сбросить очки характеристик и перераспределить их");
+
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 34;
+            Item.height = 38;
+            Item.maxStack = 9999;
+            Item.value = Item.sellPrice(gold: 1);
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.useTime = 30;
+            Item.useAnimation = 30;
+            Item.consumable = true;
+            Item.UseSound = SoundID.Item4;
+            Item.rare = ItemRarityID.Red;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            LevelPlayer modPlayer = player.GetModPlayer<LevelPlayer>();
+            return modPlayer.spentMelee + modPlayer.spentRanged + modPlayer.spentMagic + modPlayer.spentSummon + modPlayer.spentRogue + modPlayer.spentPlayer >= 0;
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            LevelPlayer modPlayer = player.GetModPlayer<LevelPlayer>();
+
+            Main.NewText("Очки характеристик сброшены! Можно перераспределить их заново.", 255, 240, 20);
+
+            // Здесь вызываем обновление интерфейса
+
+			modPlayer.ResetStats(); // Сбросить характеристики
+            return true;
+        }
+    }
+}
